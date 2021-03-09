0ae81301fa69072a9985582347e6516e711a2f0c
= <abhisanthosh@cmail.carleton.ca>
2021-03-09 16:26:19 -0500

    init

---
+++ src/main/java/Sysc4806Group/demo/AmazinApplication.java
@@ -0,0 +1,13 @@
+package Sysc4806Group.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
@@ -0,0 +1,13 @@
+package Sysc4806Group.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
