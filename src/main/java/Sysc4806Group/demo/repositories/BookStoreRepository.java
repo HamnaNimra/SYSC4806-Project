9cdc48c9acaa36508b4fbb96454083707416992f
= <abhisanthosh@cmail.carleton.ca>
2021-03-10 17:29:51 -0500

    update

--- src/main/java/Sysc4806Group/demo/repositories/BookStoreRepository.java
+++
@@ -1,7 +0,0 @@
-package Sysc4806Group.demo.repositories;
-
-import Sysc4806Group.demo.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookStoreRepository extends JpaRepository<Book, Long> {
-}
