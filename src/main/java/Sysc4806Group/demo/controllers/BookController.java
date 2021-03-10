b0a7f02f9b682cfcbc48f7366c4684db4b226c1b
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-03-10 18:40:30 -0500

    Create post mapping for upload a book

--- src/main/java/Sysc4806Group/demo/controllers/BookController.java
+++ src/main/java/Sysc4806Group/demo/controllers/BookController.java
@@ -2,7 +2,6 @@ package Sysc4806Group.demo.controllers;
 
 import Sysc4806Group.demo.entities.Book;
 import Sysc4806Group.demo.repositories.BookRepository;
-import Sysc4806Group.demo.repositories.UserRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
     private final BookRepository repository;
+
     BookController(BookRepository repository) {
         this.repository = repository;
     }
 
+    @PostMapping("/uploadBook")
+    public String uploadBook(@ModelAttribute Book book, Model model) {
+        if (!repository.exists(book.getIsbn())) {
+            repository.save(book);
+            model.addAttribute("book", book);
+            return "book";
+        } else {
+            // Prompt user that book already exists
+            return "upload-book";
+        }
+    }
+
     @PostMapping("/updateBook")
     public String updateBook(@ModelAttribute Book book, Model model) {
-
+        return "";
     }
 }
