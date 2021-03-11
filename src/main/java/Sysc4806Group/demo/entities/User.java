56471d1c5466654f9dce1c1ba7719084a5ebbc64
= <abhisanthosh@cmail.carleton.ca>
2021-03-11 14:26:20 -0500

    Works for create user

--- src/main/java/Sysc4806Group/demo/entities/User.java
+++ src/main/java/Sysc4806Group/demo/entities/User.java
@@ -7,7 +7,6 @@ import java.util.List;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String uid;
     private String firstName;
     private String lastName;
