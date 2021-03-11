b3dd69f8d3a22b2e0d7cc9bf4dd9d803192f0a64
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-03-11 11:37:17 -0500

    Update user repository id type

--- src/main/java/Sysc4806Group/demo/repositories/UserRepository.java
+++ src/main/java/Sysc4806Group/demo/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package Sysc4806Group.demo.repositories;
 import Sysc4806Group.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
