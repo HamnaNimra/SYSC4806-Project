6a9eb2ba64e11f4a692b84108d45eb6816a17cf3
= <abhisanthosh@cmail.carleton.ca>
2021-03-21 23:55:50 -0400

    added test

---
+++ src/test/java/Sysc4806Group/demo/WebMockTest.java
@@ -0,0 +1,17 @@
+package Sysc4806Group.demo;
+
+import Sysc4806Group.demo.controllers.BookController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class WebMockTest {
+
+
+}
