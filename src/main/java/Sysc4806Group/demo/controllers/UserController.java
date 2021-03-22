abe6b4a5bc9c90b5471565c6fbb2e30c61e792e2
Hamna Nimra <38963550+HamnaNimra@users.noreply.github.com>
2021-03-22 04:41:35 -0400

    updated book controller for owner

--- src/main/java/Sysc4806Group/demo/controllers/UserController.java
+++ src/main/java/Sysc4806Group/demo/controllers/UserController.java
@@ -3,15 +3,10 @@ package Sysc4806Group.demo.controllers;
 import Sysc4806Group.demo.entities.User;
 import Sysc4806Group.demo.repositories.BookRepository;
 import Sysc4806Group.demo.repositories.UserRepository;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
-
-import static Sysc4806Group.demo.entities.User.Role.OWNER;
 
 @Controller
 public class UserController {
@@ -20,20 +15,19 @@ public class UserController {
     @Autowired
     UserRepository repository;
 
-    @GetMapping("/signup")
-    public String signupForm(Model model) {
-        model.addAttribute("template", new User());
-        return "sign-up";
+    @RequestMapping("/signup")
+    public String index(Model model) {
+        return "signup";
     }
 
-    @PostMapping("/createUser")
-    public String createUser(@ModelAttribute User user, Model model,
-                             @RequestParam("firstName") String firstName,
+    @RequestMapping(value="/createUser", method=RequestMethod.GET)
+    public String createUser(@RequestParam("firstName") String firstName,
                              @RequestParam("lastName") String lastName,
                              @RequestParam("email") String email,
                              @RequestParam("password") String password,
-                             @RequestParam("role") User.Role role){
+                             @RequestParam("role") User.Role role, Model model){
 
+        User user = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setEmail(email);
@@ -41,56 +35,10 @@ public class UserController {
         user.setRole(role);
         repository.save(user);
 
-        return "main-page";
-    }
+        model.addAttribute("role",user.getRole().toString());
+        model.addAttribute("userID", user.getUid());
+        model.addAttribute("books", books.findAll());
 
-    @RequestMapping(value = "/sign-in", method = RequestMethod.GET)
-    public String signInForm(@ModelAttribute User user, Model model) {
-        model.addAttribute("template", user);
-        return "sign-in";
+        return "profile";
     }
-
-    @PostMapping("/sign-in")
-    public String signIn(@ModelAttribute User user, Model model) {
-    /*public String verifyUser(@RequestParam("email") String email,
-                @RequestParam("password") String password, HttpServletResponse response,
-                Model model)
-        List<User> users = repository.findByEmail(email);
-        //if email not in db or db empty
-        //prompt user to sign up or enter correct email
-        //take user to signIn-error page
-        // else if compare associated password for email correct verify password
-        // (user.existsById(user.getPassword()))
-        //if password correct
-        return "bookstore";
-        //else take user to signIn-error page
-        */
-        return "main-page";
-    }
-
-    @GetMapping("/manageBooks")
-    public String manageBooks(@ModelAttribute User user, Model model){
-        return "bookstoreOwner";
-    }
-
-    @GetMapping("/logout")
-    public String logout(Model model){
-        //log user out and take user to index page
-        //set session id to zero for response using cookie
-        return "index";
-    }
-
-    @GetMapping("/profile")
-    public String profile(@ModelAttribute User user,Model model){
-        //user = repository.getOne(user.getUid());
-        if(user.getRole() == null){
-            user.setRole(User.Role.CUSTOMER); //testing
-        }
-        if (user.getRole().equals(OWNER)) {
-            return "ownerProfile";
-        } else {
-            return "customerProfile";
-        }
-    }
-
 }
--- src/main/java/Sysc4806Group/demo/repositories/UserRepository.java
+++ src/main/java/Sysc4806Group/demo/repositories/UserRepository.java
@@ -2,13 +2,9 @@ package Sysc4806Group.demo.repositories;
 
 import Sysc4806Group.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "User", path = "users")
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-    List<User> findByFirstName(String firstName);
-    List<User> findByLastName(String LastName);
+public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findByEmail(String email);
 }
