d2e129abee83b8bd537f6e7e7ef7eb2ca0b220b1
= <abhisanthosh@cmail.carleton.ca>
2021-03-22 14:27:11 -0400

    basic test to add book

--- src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
+++ src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
@@ -1,7 +1,12 @@
 package Sysc4806Group.demo;
 
 import Sysc4806Group.demo.controllers.BookController;
+import Sysc4806Group.demo.entities.Book;
+import Sysc4806Group.demo.entities.User;
 import Sysc4806Group.demo.repositories.BookRepository;
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.io.entity.EntityUtils;
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +20,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.client.RestTemplate;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectWriter;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,6 +43,8 @@ class AmazinApplicationTests {
 	void contextLoads() {
 	}
 
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -44,7 +58,19 @@ class AmazinApplicationTests {
 	}
 
 	@Test
-	public void searchWithItems() throws Exception{
+	public void searchWithItemsTest() throws Exception{
+		String url = "http://localhost:8080" + "/uploadBook";
+
+		mockMvc.perform(post(url)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.content(EntityUtils.toString(new UrlEncodedFormEntity(Arrays.asList(
+						new BasicNameValuePair("isbn","1234"),
+						new BasicNameValuePair("author","Abhiram Santhosh"),
+						new BasicNameValuePair("title", "The inheritance of rome"),
+						new BasicNameValuePair("publisher","Penguin books"),
+						new BasicNameValuePair("inventory","1"),
+						new BasicNameValuePair("pictureUrl", "blank")
+				))))).andDo(print()).andExpect(status().isOk());
 
 	}
 
