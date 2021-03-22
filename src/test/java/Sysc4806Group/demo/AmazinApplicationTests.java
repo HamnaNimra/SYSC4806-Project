2fa562e146336d36867a1d2a68f39fc142215877
= <abhisanthosh@cmail.carleton.ca>
2021-03-21 22:57:17 -0400

    test init

--- src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
+++ src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
@@ -1,13 +1,33 @@
 package Sysc4806Group.demo;
 
+import Sysc4806Group.demo.controllers.BookController;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 class AmazinApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getSearchResults() throws Exception {
+
+		
+
 	}
 
 }
