401d53bc1b5a16272291076d8eb48a51a3ae24a9
Hamna Nimra <38963550+HamnaNimra@users.noreply.github.com>
2021-04-09 01:35:44 -0400

    integration tests modified

--- src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
+++ src/test/java/Sysc4806Group/demo/AmazinApplicationTests.java
@@ -1,7 +1,9 @@
 package Sysc4806Group.demo;
 
+import Sysc4806Group.demo.entities.Role;
 import Sysc4806Group.demo.repositories.BookRepository;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.Header;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.io.entity.EntityUtils;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.junit.Before;
@@ -15,12 +17,13 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.nio.charset.Charset;
 import java.util.Arrays;
-import static org.hamcrest.Matchers.containsString;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,14 +42,10 @@ class AmazinApplicationTests {
     @Autowired
     private BookRepository repository;
 
-    @Before
-    void setup(){}
-
-
     @Test
     public void getSignup() throws Exception {
         MvcResult result = this.userMvc.perform(get("/signup")
-                .contentType(MediaType.TEXT_HTML))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
         String contents = result.getResponse().getContentAsString();
@@ -59,27 +58,48 @@ class AmazinApplicationTests {
 
     @Test
     public void postSignup() throws Exception {
-
+        userMvc.perform(post("/signup")
+                .param("First Name", "Hamna")
+                .param("Last Name", "Nimra")
+                .param("Email", "test@gmail.com")
+                .param("Password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isFound())
+                .andReturn();
     }
 
-    /*@Test
-    public void searchWithItemsTest() throws Exception {
-        String url = "http://localhost:8080" + "/uploadBook";
+    @Test
+    public void getSignIn() throws Exception {
+        MvcResult result = this.userMvc.perform(get("/signin")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String contents = result.getResponse().getContentAsString();
+        assert (contents.contains("Email"));
+        assert (contents.contains("Password"));
+    }
 
-        mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(EntityUtils.toString(new UrlEncodedFormEntity(Arrays.asList(
-                        new BasicNameValuePair("isbn", "1234"),
-                        new BasicNameValuePair("author", "Abhiram Santhosh"),
-                        new BasicNameValuePair("title", "The inheritance of rome"),
-                        new BasicNameValuePair("publisher", "Penguin books"),
-                        new BasicNameValuePair("inventory", "1"),
-                        new BasicNameValuePair("pictureUrl", "blank")
-                ))))).andExpect(status().isOk()).andDo(
+    @Test
+    public void postSignIn() throws Exception {
+        userMvc.perform(post("/signin")
+                .param("Email", "test@gmail.com")
+                .param("Password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isFound())
+                .andReturn();
+    }
 
-                result -> mockMvc.perform(get("/search?title=rome")).andDo(print()).andExpect(
-                        content().string(containsString("1234"))
-                )
-        );
-    }*/
+    @Test
+    public void postUploadBook() throws Exception {
+        userMvc.perform(post("/uploadBook")
+                .param("Title", "XYZ Book")
+                .param("ISBN", "123892")
+                .param("Author", "West Author")
+                .param("Publisher", "StudentsPub")
+                .param("Picture URL", "google.com/images")
+                .param("Inventory Num", "15")
+                .with(csrf()))
+                .andExpect(status().isFound())
+                .andReturn();
+    }
 }
