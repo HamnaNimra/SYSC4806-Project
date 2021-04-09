7be824ffd58e63e881aa80e023a3fdbdb0d91235
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-04-08 20:16:46 -0400

    Revert "Merge pull request #13 from HamnaNimra/hamnanimra5110/ch20/admin-owner-user-access"
    This reverts commit 76ca718325074c3eac10fb93bb4b13638de9e94f, reversing
    changes made to 21c2b48ce46420385d4452f2306181908fa26f6e.

--- src/main/java/Sysc4806Group/demo/repositories/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package Sysc4806Group.demo.repositories;
-
-import Sysc4806Group.demo.entities.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
